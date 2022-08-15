ce7f5cde106046a198c1edc608a427ecd254d497
Zvonimir Pervan <z.pervan65@gmail.com>
2022-08-15 14:14:07 +0200

    Make app buildable on Ubuntu

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 #include <stdio.h>
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
 #include <GLFW/glfw3.h> // Will drag system OpenGL headers
 
 static void glfw_error_callback(int error, const char* description)
