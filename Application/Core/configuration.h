27c447c426f07a895a5b784c2f0f0379f327ee09
Zvonimir Pervan <z.pervan65@gmail.com>
2022-08-20 13:13:50 +0200

    Change font size

--- Application/Core/configuration.h
+++ Application/Core/configuration.h
@@ -5,7 +5,7 @@ namespace Configuration
 {
 static constexpr auto WINDOW_WIDTH{1800.0f};
 static constexpr auto WINDOW_HEIGHT{900.0f};
-static constexpr auto FONT_SIZE{21.0f};
+static constexpr auto FONT_SIZE{18.0f};
 static constexpr auto MENU_BAR_HEIGHT{FONT_SIZE + 5.0f};
 }
 
