d6ed2c994d5a44c148771be9237f13b1dccfdfeb
Zvonimir Pervan <z.pervan65@gmail.com>
2022-09-06 11:21:23 +0200

    Add custom button layout

---
+++ Application/GUI/Elements/button.cpp
@@ -0,0 +1,62 @@
+#include "button.h"
+
+GUI::Button::Button(EventSystem& event_system)
+    : event_system_(event_system), size_({120, 30}), x_position_(0.0f), y_position_(0.0f)
+{
+}
+
+void GUI::Button::SetText(const std::string& button_text)
+{
+    id_ = Id::GenerateIdWithLabel(button_text);
+    button_text_ = button_text;
+}
+
+void GUI::Button::HorizontalAlignment(ButtonHorizontalAlignment button_horizontal_alignment, ImVec2 parent_window_size)
+{
+    assert(parent_window_size.x > 0.0f && parent_window_size.y > 0.0f && "Parent window size must be valid");
+
+    if (ButtonHorizontalAlignment::Left == button_horizontal_alignment)
+    {
+        x_position_ = (parent_window_size.x - size_.x) * (1.0f / 8.0f);
+    }
+
+    if (ButtonHorizontalAlignment::Center == button_horizontal_alignment)
+    {
+        x_position_ = (parent_window_size.x - size_.x) * (4.0f / 8.0f);
+    }
+
+    if (ButtonHorizontalAlignment::Right == button_horizontal_alignment)
+    {
+        x_position_ = (parent_window_size.x - size_.x) * (7.0f / 8.0f);
+    }
+}
+
+void GUI::Button::VerticalAlignment(ButtonVerticalAlignment button_vertical_alignment, ImVec2 parent_window_size)
+{
+    assert(parent_window_size.x > 0.0f && parent_window_size.y > 0.0f && "Parent window size must be valid");
+
+    if (ButtonVerticalAlignment::Up == button_vertical_alignment)
+    {
+        y_position_ = (parent_window_size.y - size_.y) * (1.0f / 8.0f);
+    }
+
+    if (ButtonVerticalAlignment::Center == button_vertical_alignment)
+    {
+        y_position_ = (parent_window_size.y - size_.y) * (4.0f / 8.0f);
+    }
+
+    if (ButtonVerticalAlignment::Down == button_vertical_alignment)
+    {
+        y_position_ = (parent_window_size.y - size_.y) * (7.0f / 8.0f);
+    }
+}
+
+bool GUI::Button::Show()
+{
+    assert(!button_text_.empty() && "Button text must be set before usage");
+
+    ImGui::SetCursorPosX(x_position_);
+    ImGui::SetCursorPosY(y_position_);
+
+    return ImGui::Button(id_.c_str(), size_);
+}
---
+++ Application/GUI/Elements/button.h
@@ -0,0 +1,52 @@
+#ifndef STATICCALCULATION_BUTTON_H
+#define STATICCALCULATION_BUTTON_H
+
+#include <imgui.h>
+
+#include <string>
+
+#include "Application/Core/event_system.h"
+#include "Application/GUI/Core/id.h"
+
+namespace GUI
+{
+
+enum class ButtonHorizontalAlignment
+{
+    Left = 0,
+    Center,
+    Right
+};
+
+enum class ButtonVerticalAlignment
+{
+    Up = 0,
+    Center,
+    Down
+};
+
+class Button
+{
+  public:
+    Button(EventSystem& event_system);
+
+    void SetText(const std::string& button_text);
+    void HorizontalAlignment(GUI::ButtonHorizontalAlignment button_horizontal_alignment, ImVec2 parent_window_size);
+    void VerticalAlignment(GUI::ButtonVerticalAlignment button_vertical_alignment, ImVec2 parent_window_size);
+
+    bool Show();
+
+  private:
+    EventSystem& event_system_;
+
+    ImVec2 size_;
+    float x_position_;
+    float y_position_;
+
+    std::string button_text_;
+    std::string id_;
+};
+
+}  // namespace GUI
+
+#endif  // STATICCALCULATION_BUTTON_H
