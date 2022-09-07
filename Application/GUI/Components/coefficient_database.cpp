91a79d4636896fa42653cddc561cf96b3e3c9c68
Zvonimir Pervan <z.pervan65@gmail.com>
2022-09-07 13:13:59 +0200

    List coefficints in coefficient database window

--- Application/GUI/Components/coefficient_database.cpp
+++ Application/GUI/Components/coefficient_database.cpp
@@ -45,6 +45,41 @@ void CoefficientDatabase::Show()
 
         coefficient_group_combo_box_.Show(*coefficient_service_.GetCoefficientsDatabase(), selected_coefficient_group_);
 
+        if (ImGui::BeginTable(table_id.c_str(), 3, ImGuiTableFlags_Borders))
+        {
+            ImGui::TableSetupColumn("Parametar");
+            ImGui::TableSetupColumn("Vrijednost");
+            ImGui::TableSetupColumn("", ImGuiTableFlags_NoBordersInBody);
+            ImGui::TableHeadersRow();
+
+            std::size_t component_id{0};
+
+            auto* selected_database{&(*coefficient_service_.GetCoefficientsDatabase())[selected_coefficient_group_]};
+
+            for (auto it = selected_database->begin(); it != selected_database->end(); ++it)
+            {
+                ImGui::TableNextRow();
+
+                ImGui::TableSetColumnIndex(0);
+                ImGui::Text("%s", it->first.c_str());
+
+                ImGui::TableSetColumnIndex(1);
+                ImGui::Text("%.2f", it->second);
+
+                ImGui::TableSetColumnIndex(2);
+                std::string delete_label_id{fmt::format("X##{}", component_id)};
+                if (ImGui::Button(delete_label_id.c_str(), {20.0f, 0.0f}))
+                {
+                    /// @TODO: Implement deleting rows and reflect it in coefficient JSON file
+                    // row_to_remove_ = it;
+                };
+
+                ++component_id;
+            }
+
+            ImGui::EndTable();
+        }
+
         if (save_button_.Show())
         {
             spdlog::debug("Implement saving functionality");
